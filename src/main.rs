ba2b16f789678b1420f6fd0dfb669fced9935927
kostaskoukos <kkonst2014@gmail.com>
2024-11-24 16:17:28 +0200

    add basic error handling & byte count

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::{fs, process};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().skip(1).collect();
+    if args.is_empty() {
+        eprintln!("Wrong arguments provided");
+        process::exit(1);
+    }
+    let path = &args[0];
+    let file = fs::read_to_string(path).unwrap_or_else(|e| {
+        eprintln!("An error occurred while reading the file: {e}");
+        process::exit(1);
+    });
+
+    println!("{} {}", file.bytes().len(), path);
 }
