33666103621cbf7e86345df28abd77140ed6fc31
kostaskoukos <kkonst2014@gmail.com>
2024-12-08 18:42:42 +0200

    implement all functionality

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::process;
+use std::{error::Error, fs, process};
 
 enum CountOptions {
     Bytes,
@@ -31,10 +31,38 @@ impl Config {
     }
 }
 
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let str = fs::read_to_string(config.path)?;
+    if config.counts.is_empty() {
+        println!(
+            "Bytes: {} Characters: {} Words: {} Lines: {}",
+            str.bytes().len(),
+            str.chars().count(),
+            str.split_ascii_whitespace().count(),
+            str.lines().count()
+        )
+    } else {
+        for count in config.counts {
+            match count {
+                CountOptions::Bytes => print!("Bytes: {} ", str.bytes().len()),
+                CountOptions::Chars => print!("Characters: {} ", str.chars().count()),
+                CountOptions::Words => print!("Words: {} ", str.split_ascii_whitespace().count()),
+                CountOptions::Lines => print!("Lines: {} ", str.lines().count()),
+            }
+        }
+        println!()
+    }
+    Ok(())
+}
+
 fn main() {
     let args: Vec<String> = std::env::args().skip(1).collect();
     let config = Config::new(&args).unwrap_or_else(|| {
         eprintln!("Wrong arguments provided");
         process::exit(1);
     });
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
 }
